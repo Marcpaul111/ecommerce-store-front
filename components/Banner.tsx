a6031c2eacfc0dcc142185aa09a54bffbb746879
Marco Lazaro <lazaromarco1526@gmail.com>
2024-10-23 14:11:16 +0800

    type error fix

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,18 +1,18 @@
 import { Banner as BannerType } from "@/types"
 
 interface BannerProps{
-    data: BannerType
+    data: BannerType | null
 }
 
 const Banner: React.FC<BannerProps> = ({data}) => {
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
         <div className="rounded-xl lg:p-8 aspect-square md:aspect-[2.4/1] lg:aspect-[3.5/1] overflow-hidden bg-cover" 
-        style={{backgroundImage: `url(${data.imageUrl})`, backgroundSize: ""}}
+        style={{backgroundImage: `url(${data?.imageUrl})`, backgroundSize: ""}}
         >
             <div className="h-full flex flex-col justify-center items-center text-center gap-y-8">
                 <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xs">
-                    {data.label}
+                    {data?.label}
                 </div>
             </div>
         </div>
