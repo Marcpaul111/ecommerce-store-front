cb04d95044a70e4cbeed7721e0b9e66a78d17f54
Marco Lazaro <lazaromarco1526@gmail.com>
2024-11-04 14:24:21 +0800

    fix label

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,23 +1,30 @@
-import { Banner as BannerType } from "@/types"
+import { Banner as BannerType } from "@/types";
+import { Separator } from "./ui/separator";
 
-interface BannerProps{
-    data: BannerType | null
+interface BannerProps {
+  data: BannerType | null;
 }
 
-const Banner: React.FC<BannerProps> = ({data}) => {
+const Banner: React.FC<BannerProps> = ({ data }) => {
   return (
-    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-        <div className="rounded-xl lg:p-8 aspect-square md:aspect-[2.4/1] lg:aspect-[3.5/1] overflow-hidden bg-cover" 
-        style={{backgroundImage: `url(${data?.imageUrl})`, backgroundSize: ""}}
-        >
-            <div className="h-full flex flex-col justify-center items-center text-center gap-y-8">
-                <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xs">
-                    {data?.label}
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+    <>
+      <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+        <div
+          className="rounded-xl lg:p-8 aspect-square md:aspect-[2.4/1] lg:aspect-[3.5/1] overflow-hidden bg-cover"
+          style={{
+            backgroundImage: `url(${data?.imageUrl})`,
+            backgroundSize: "",
+          }}
+        ></div>
+      </div>
+      <div className="relative flex items-center justify-center my-10">
+        <Separator className="absolute top-1/2 w-full transform -translate-y-1/2 text-lg bg-gray-900" />
+        <h3 className="relative z-10 font-bold text-3xl bg-white text-center px-10">
+          {data?.label}
+        </h3>
+      </div>
+    </>
+  );
+};
 
-export default Banner
+export default Banner;
